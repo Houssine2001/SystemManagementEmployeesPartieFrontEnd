1fd14048ed0c115e679b31aeb94061c5df3020e6
Houssine2001 <Houssine.BenArous@esprit.tn>
2024-11-30 21:49:43 +0100

    Premier commit du FrontEnd

---
+++ angular/src/app/services/employee.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private apiUrl = 'http://localhost:8081/api/employees'; // URL de l'API Spring Boot
+
+  constructor(private http: HttpClient) { }
+
+  // Méthode pour récupérer un employé par ID avec authentification basique
+  getEmployeeById(id: string): Observable<any> {
+    // Ajout de l'en-tête Authorization pour l'authentification basique
+    const headers = new HttpHeaders({
+      'Authorization': 'Basic YWRtaW46YjQ1OTRlOWEtMjY0OC00N2IwLWE3ZmItODRiNDVhZmFlNmY2' // Remplacez par vos credentials encodés
+    });
+
+    return this.http.get(`${this.apiUrl}/${id}`, { headers });
+  }
+}
